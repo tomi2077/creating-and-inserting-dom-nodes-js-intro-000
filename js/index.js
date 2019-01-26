c498b574d86d2794fef2e3561653a749f31a9844
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-26 13:52:54 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
 //this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+//you can use this to paste in js code and have it run when index.html is loaded..
